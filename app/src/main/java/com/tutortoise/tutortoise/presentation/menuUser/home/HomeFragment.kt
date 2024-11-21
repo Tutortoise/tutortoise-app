f68b456c85d188b41b9450c59fed919da2cb558d
elskow <elskow@outlook.com>
2024-11-21 19:15:03 +0700

    refactor(layout): Rename fragments to distinguish between learner and tutor views

--- app/src/main/java/com/tutortoise/tutortoise/presentation/menuUser/home/HomeFragment.kt
+++ app/src/main/java/com/tutortoise/tutortoise/presentation/menuUser/home/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.tutortoise.tutortoise.presentation.menuUser.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.tutortoise.tutortoise.R
 
 class HomeFragment : Fragment() {
@@ -13,6 +13,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_learner_home, container, false)
     }
 }
