9761de641a09b504c20acf39d28e6f6b9feed5e0
elskow <elskow@outlook.com>
2024-11-22 14:20:17 +0700

    chore: update layout references in ExploreFragment and navigation

--- app/src/main/java/com/tutortoise/tutortoise/presentation/menuLearner/explore/ExploreFragment.kt
+++ app/src/main/java/com/tutortoise/tutortoise/presentation/menuLearner/explore/ExploreFragment.kt
@@ -14,6 +14,6 @@ class ExploreFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        return inflater.inflate(R.layout.fragment_learner_explore, container, false)
     }
 }
