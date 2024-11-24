6063229bb5f5afb8c5f3dbd879c8aaae28493d8e
IkkiCantCode <mrizkynoob@gmail.com>
2024-11-24 18:59:07 +0800

    feat: CreateServiceActivity

---
+++ app/src/main/java/com/tutortoise/tutortoise/presentation/menuTutor/tutories/SetScheduleActivity.kt
@@ -0,0 +1,15 @@
+package com.tutortoise.tutortoise.presentation.menuTutor.tutories
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.tutortoise.tutortoise.R
+
+class SetScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_schedule)
+    }
+}
