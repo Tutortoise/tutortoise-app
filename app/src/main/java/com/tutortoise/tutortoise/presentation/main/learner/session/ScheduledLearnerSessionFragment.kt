17627bae9d79a84843101aea35f8c32a15833864
elskow <elskow@outlook.com>
2024-12-10 09:00:17 +0700

    feat: add no booked sessions illustration

--- app/src/main/java/com/tutortoise/tutortoise/presentation/main/learner/session/ScheduledLearnerSessionFragment.kt
+++ app/src/main/java/com/tutortoise/tutortoise/presentation/main/learner/session/ScheduledLearnerSessionFragment.kt
@@ -1,10 +1,12 @@
 package com.tutortoise.tutortoise.presentation.main.learner.session
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -32,6 +34,7 @@ class ScheduledLearnerSessionFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -72,6 +75,7 @@ class ScheduledLearnerSessionFragment : Fragment() {
         viewModel.fetchMyOrders("scheduled")
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         super.onResume()
         viewModel.fetchMyOrders("scheduled")
